5e84cb4a5ee6899a237470e9ec1f95b1f4049a74
Kevin Rodríguez <kyrodriguez97@gmail.com>
2017-01-08 19:50:34 -0500

    project bases

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,8 @@
-
+function responsiveNav() {
+    var nav = document.getElementById("nav-list");
+    if (nav.className === "nav-list") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "nav-list";
+    }
+}
